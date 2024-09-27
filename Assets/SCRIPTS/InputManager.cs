09d0bae5619e687ed6f448e41c6180aad84fde25
MateoMonastra <mateomonastravm@gmail.com>
2024-09-26 23:17:30 -0300

    Add input & tutorial mobileart

---
+++ Assets/SCRIPTS/InputManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager
+{
+    private static InputManager _instance;
+
+    public static InputManager GetInstance()
+    {
+        if (_instance != null)
+        {
+            return _instance;
+        }
+
+        _instance = new InputManager();
+        return _instance;
+    }
+
+    private Dictionary<string, float> _axisValues = new Dictionary<string, float>();
+
+    public void SetAxis(string axisName, float value)
+    {
+        _axisValues.TryAdd(axisName, value);
+
+        _axisValues[axisName] = value;
+    }
+
+    public float GetAxis(string inputName)
+    {
+#if UNITY_EDITOR
+        return GetOrAddAxis(inputName) + Input.GetAxis(inputName);
+#endif
+#if UNITY_ANDROID || UNITY_IOS
+        return GetOrAddAxis(inputName);
+#endif
+#if UNITY_STANDALONE
+        return Input.GetAxis(inputName);
+#endif
+    }
+
+    public float GetOrAddAxis(string axisName)
+    {
+        _axisValues.TryAdd(axisName, 0);
+        return _axisValues[axisName];
+    }
+}
---
+++ Assets/SCRIPTS/VirtualJoystick.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private RectTransform stick = null;
+
+    public string player = "";
+    public float limit = 80.0f;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        stick.anchoredPosition = ConverToLocal(eventData);
+    }
+
+    private Vector2 ConverToLocal(PointerEventData eventData)
+    {
+        Vector2 newPos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle
+        (transform as RectTransform,
+            eventData.position,
+            eventData.enterEventCamera,
+            out newPos);
+        return newPos;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("draggued");
+        Vector2 pos = ConverToLocal(eventData);
+        if (pos.magnitude > limit)
+            pos = pos.normalized * limit;
+
+        stick.anchoredPosition = pos;
+
+        float x = pos.x / limit;
+        float y = pos.y / limit;
+
+        SetHorizontal(x);
+        SetVertical(y);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("zero");
+        stick.anchoredPosition = Vector2.zero;
+        SetHorizontal(0f);
+        SetVertical(0f);
+    }
+
+    private void OnDisable()
+    {
+        SetHorizontal(0f);
+        SetVertical(0f);
+    }
+
+    private void SetHorizontal(float val) { InputManager.GetInstance().SetAxis($"Horizontal{player}", val); }
+
+    private void SetVertical(float val) { InputManager.GetInstance().SetAxis($"Horizontal{player}", val); }
+}
